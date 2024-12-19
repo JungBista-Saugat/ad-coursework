f5cce1268a442691fe970d91118a59fe9c81fe93
Saugat Jung Bista <134186923+JungBista-Saugat@users.noreply.github.com>
2024-12-19 09:53:11 +0545

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ad_coursework
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ad_coursework
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
